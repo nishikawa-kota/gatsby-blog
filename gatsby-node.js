5592899e978d898be8100940dcf50bd6e3f1bd38
kota nishikawa <koonii26@gmail.com>
2021-04-28 01:09:18 +0900

    動く状態に戻す

--- gatsby-node.js
+++ gatsby-node.js
@@ -36,4 +36,25 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       context: {}, // additional data can be passed via context
     })
   })
+  //以下を動かすとエラーが出る
+  // const postsPerPage = 4
+  // const numberOfPages = Math.ceil(posts.length / postsPerPage)
+
+  // Array.from({ length: numberOfPages }).forEach((_, index) => {
+  //   const isFirstPage = index === 0
+  //   const currentPage = index + 1
+  //   // Skip first page because of index.js
+  //   if (isFirstPage) return
+
+  //   createPage({
+  //     path: `/page/${currentPage}`,
+  //     component: blogPostTemplate,
+  //     context: {
+  //       limit: postsPerPage,
+  //       skip: index * postsPerPage,
+  //       numberOfPages: numberOfPages,
+  //       currentPage: currentPage,
+  //     },
+  //   })
+  // })
 }
